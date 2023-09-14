6249195d697720036e21fb2329b8e3ef6f414ef1
DennyKuo <45343949+DennyKuo0809@users.noreply.github.com>
2023-09-14 08:08:05 +0800

    Best Time to Buy and Sell Stock

---
+++ DP/easy/121.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price = prices[0];
+        int res = 0;
+        for(int i = 1 ; i < prices.size() ; i ++){
+            res = max(res, prices[i] - min_price);
+            min_price = min(min_price, prices[i]);
+        }
+        return res;
+    }
+};
